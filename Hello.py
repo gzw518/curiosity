31c8e5226484e3112f53aa4326f79d3724b4d491
Eric Guo <gzw_518@163.com>
2023-09-28 03:16:19 +0000

    add gitignore

--- Hello.py
+++ Hello.py
@@ -42,7 +42,7 @@ if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?")
     search = DuckDuckGoSearchRun(name="Search")
     search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
     with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
+        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=True)
         response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
         st.session_state.messages.append({"role": "assistant", "content": response})
         st.write(response)
